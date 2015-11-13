11a544063f9aac912d5efbeea4eed2157ef53173
Derek Gonyeo <derek@gonyeo.com>
2015-11-13 09:33:56 -0800

    fsdiffer: BeforeAfterFSDiffer renamed to TemporalFSDiffer
    Also moved the Start function into NewTemporalFSDiffer

--- util/fsdiffer/temporal_test.go
+++ util/fsdiffer/temporal_test.go
@@ -1,3 +1,16 @@
+// Copyright 2015 The appc Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package fsdiffer
 
 import (
@@ -11,7 +24,7 @@ import (
 )
 
 // Do not test (at the moment) symlinks as every os needs a special syscall to set symlink times
-func TestBeforeAfterFSDiffer(t *testing.T) {
+func TestTemporalFSDiffer(t *testing.T) {
 	time1 := time.Now()
 	time2 := time.Now().Add(1 * time.Microsecond)
 	sourceFiles := []*buildFileInfo{
@@ -85,8 +98,7 @@ func TestBeforeAfterFSDiffer(t *testing.T) {
 		if err != nil {
 			t.Fatalf("unexpected error: %v", err)
 		}
-		ba := NewBeforeAfterFSDiffer(testDir)
-		err = ba.Start()
+		tm, err := NewTemporalFSDiffer(testDir)
 		if err != nil {
 			t.Fatalf("unexpected error: %v", err)
 		}
@@ -97,7 +109,7 @@ func TestBeforeAfterFSDiffer(t *testing.T) {
 			t.Fatalf("unexpected error: %v", err)
 		}
 
-		changes, err := ba.Diff()
+		changes, err := tm.Diff()
 		changesMap := changes.ToMap()
 		if err != nil {
 			t.Errorf("unexpected error: %v", err)
