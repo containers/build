7fe75712342681d2405217a0010295ac3617535b
Derek Gonyeo <derek.gonyeo@coreos.com>
2015-11-30 21:46:14 +0000

    begin: when given a directory, use as initial rootfs
    To allow for users to specify an entire rootfs for their ACI, support
    has been added to the begin command to allow for starting from a given
    directory. This results in a non-empty rootfs, and an empty manifest.

--- tests/begin_test.go
+++ tests/begin_test.go
@@ -22,6 +22,7 @@ import (
 	"path"
 	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/appc/acbuild/Godeps/_workspace/src/github.com/appc/spec/aci"
 )
@@ -82,4 +83,32 @@ func TestBeginLocalACI(t *testing.T) {
 
 	checkManifest(t, workingDir, detailedManifest())
 	checkEmptyRootfs(t, workingDir)
+}
+
+func TestBeginLocalDirectory(t *testing.T) {
+	sourceDir, err := ioutil.TempDir("", "acbuild-test-begin")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(sourceDir)
+
+	time1 := time.Now()
+	files := []*buildFileInfo{
+		mkBuildFileInfoFile("file01", time1),
+		mkBuildFileInfoDir("dir01", time1),
+		mkBuildFileInfoFile("dir01/file01", time1),
+	}
+
+	mustBuildFS(sourceDir, files)
+
+	workingDir := mustTempDir()
+	defer cleanUpTest(workingDir)
+
+	_, _, _, err = runACBuild(workingDir, "begin", sourceDir)
+	if err != nil {
+		t.Fatalf("%s\n", err.Error())
+	}
+
+	checkManifest(t, workingDir, emptyManifest())
+	testMatchingFSTree(t, workingDir, sourceDir, "/")
 }
